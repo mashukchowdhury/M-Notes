f4dfe6d447bce597b8ded26091c756329e50d38c
Mashuk Chowdhury <76137247+mashukchowdhury@users.noreply.github.com>
2021-12-04 13:17:16 -0400

    Created Adapter
    For TItles and Content for Main body

--- app/src/main/java/com/nscc/firenotes/MainActivity.java
+++ app/src/main/java/com/nscc/firenotes/MainActivity.java
@@ -1,20 +1,28 @@
 package com.nscc.firenotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView nav_view;
+    RecyclerView noteLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +32,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        noteLists = findViewById(R.id.noteList);
         drawerLayout = findViewById(R.id.drawer);
         nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            default:
+                Toast.makeText(this,"Coming Soon.", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings){
+            Toast.makeText(this,"Settings Menu has been Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/model/Adapter.java
@@ -0,0 +1,48 @@
+package model;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nscc.firenotes.R;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    List<String>titles;
+    List<String> content;
+
+    public Adapter(List<String> title,List<String> content)
+    {
+        this.titles = title;
+        this.content = content;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
